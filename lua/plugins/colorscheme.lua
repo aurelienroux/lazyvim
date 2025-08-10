3115288c3b90766634e4315a92d4c07cefa00c02
Aurélien Roux <aureroux13@gmail.com>
2025-08-10 18:51:34 +0200

    correctly load onedarkpro theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  { "olimorris/onedarkpro.nvim", priority = 1000 }, -- install theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vaporwave", -- tell LazyVim to use it
+    },
+  },
+}
